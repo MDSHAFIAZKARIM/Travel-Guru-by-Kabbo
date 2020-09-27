5672acc6558712a189ca407c85136180b5613543
MDSHAFIAZKARIM <67519239+MDSHAFIAZKARIM@users.noreply.github.com>
2020-09-28 01:29:03 +0600

    first commit

---
+++ src/firebaseconfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlnHzAMlC3LV25sw_svapGcKCbR7JOBGY",
+    authDomain: "travel-guru-544a1.firebaseapp.com",
+    databaseURL: "https://travel-guru-544a1.firebaseio.com",
+    projectId: "travel-guru-544a1",
+    storageBucket: "travel-guru-544a1.appspot.com",
+    messagingSenderId: "333935124519",
+    appId: "1:333935124519:web:020c4ba5e97a9adb68ae02"
+  };
+  export default firebaseConfig;
